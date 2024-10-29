d31edb22b64429cd1ea15bca23c04362e4855d12
hahas94 <harady.hasan@gmail.com>
2024-10-29 11:19:48 +0100

    Automating the algorithm template file creation even more.

--- algo_template.sh
+++ algo_template.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Use this file for generating template code for implementation of an algorithm.
-# It will create two files, `algorithm/algo_name.cpp` and `algorithm/algo_name.hpp`,
+# It will create two files, `algorithms/algo_name.cpp` and `algorithms/algo_name.hpp`,
 # as well as updating the relevant cmake files.
 
 # Run from root of project.
 # Usage: ./algo_template.sh algo_name
 
 file_name=$1  
+filename_uppercase=$(echo "$file_name" | tr '[:lower:]' '[:upper:]')
 
 header_file="$file_name.hpp"
 src_file="$file_name.cpp"
@@ -19,10 +20,15 @@ cat <<EOT >> algorithms/$header_file
  * $header_file
  * ------------------
  * Description:
- *   Header file containing declarations for the $file_name problem. 
+ *   Header file containing declarations for the $file_name algorithm. 
  * */
+
+#ifndef ${filename_uppercase}_HPP
+#define ${filename_uppercase}_HPP
  
 #include <iostream>
+
+#endif
 
 // ============== END OF FILE ==============
 
@@ -32,7 +38,7 @@ EOT
 touch "algorithms/$src_file"
 cat <<EOT >> algorithms/$src_file
 /**
- * $implementation_file
+ * $src_file
  * ------------------
  * Description:
  *   Implementation file containing definitions for the $file_name problem. 
