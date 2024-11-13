eb0ea45660f9e8c99b318ba1495f9c1c7b385878
hahas94 <harady.hasan@gmail.com>
2024-11-13 15:04:32 +0100

    documenting class methods.

--- algorithms/bit.cpp
+++ algorithms/bit.cpp
@@ -13,11 +13,22 @@
 
 #include "bit.hpp"
 
+/**
+ * @brief Construct a new BIT::BIT object
+ * 
+ * @param n: Number of array elements. 
+ */
 BIT::BIT(std::size_t n)
     : N{n+1}, tree(n+1) {}
 
 BIT::~BIT(){}
 
+/**
+ * @brief Incrementing an element in the array.
+ * 
+ * @param index: position of element 
+ * @param delta: value to increment by. 
+ */
 void BIT::add(std::size_t index, int delta){
     checkIndex(index);
     index += 1;  // adjusting to the tree size
@@ -27,6 +38,12 @@ void BIT::add(std::size_t index, int delta){
     }
 }
 
+/**
+ * @brief Query the sum of array elements from position 0 up to index-1.
+ * 
+ * @param index: an index to query the cumulative sum at. 
+ * @return long long int: the cumulative sum. 
+ */
 long long int BIT::sum(std::size_t index) const{
     checkIndex(index);
     long long int sum {0};
@@ -39,11 +56,23 @@ long long int BIT::sum(std::size_t index) const{
     return sum;
 }
 
+/**
+ * @brief Print the binary indexed tree.
+ * 
+ * @param os: output stream 
+ * @param data: a tree. 
+ * @return std::ostream& 
+ */
 std::ostream& operator<<(std::ostream& os, const BIT& data){
    std::copy(data.tree.begin(), data.tree.end(), std::ostream_iterator<int>(os, " "));
     return os;
 }
 
+/**
+ * @brief Check whether a given index is within range.
+ * 
+ * @param index 
+ */
 constexpr void BIT::checkIndex(std::size_t index) const{
     if(index > N){
         throw std::out_of_range("Index" + std::to_string(index) + " is out of range.");
